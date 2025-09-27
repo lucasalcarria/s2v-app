3eb8bab0e5ceeebb26baf071ca0092e324c9b27f
Rodolpho Toppan <rodolphotoppan@hotmail.com>
2025-09-27 19:34:35 -0300

    feature: atualizando o link da API para https://relationships-helen-earthquake-electricity.trycloudflare.com

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,4 +1,4 @@
-const API_BASE_URL = "http://192.168.0.13:8080/";
+const API_BASE_URL = "https://relationships-helen-earthquake-electricity.trycloudflare.com/";
 
 export async function apiFetch<T>(
   endpoint: string,
