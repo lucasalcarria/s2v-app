c2b734e7ff1c876dc87c2030218be8251ca94537
Rodolpho Toppan <rodolphotoppan@hotmail.com>
2025-09-29 13:12:10 -0300

    feature: atualizando o link da API para https://suggested-return-ant-photo.trycloudflare.com

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,4 +1,4 @@
-const API_BASE_URL = "https://effectiveness-recognize-occasions-julie.trycloudflare.com/";
+const API_BASE_URL = "https://suggested-return-ant-photo.trycloudflare.com/";
 
 export async function apiFetch<T>(
   endpoint: string,
