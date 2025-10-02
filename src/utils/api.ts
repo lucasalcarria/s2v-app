b6ff18494396c44134d4ae625d1c79c47043a6df
lucasalcarria <lucasalcarria@gmail.com>
2025-10-02 11:44:50 -0300

    feature: atualizando o link da API para https://cluster-library-composition-geometry.trycloudflare.com

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,4 +1,4 @@
-const API_BASE_URL = "https://constitutes-electric-musician-addresses.trycloudflare.com/";
+const API_BASE_URL = "https://cluster-library-composition-geometry.trycloudflare.com/";
 
 export async function apiFetch<T>(
   endpoint: string,
