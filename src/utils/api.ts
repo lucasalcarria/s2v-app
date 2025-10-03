79b3af341d98765b7755bc0cab2875decb801c49
lucasalcarria <lucasalcarria@gmail.com>
2025-10-03 10:56:02 -0300

    feature: atualizando o link da API para https://elect-tube-legislative-casting.trycloudflare.com

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,4 +1,4 @@
-const API_BASE_URL = "https://rubber-pit-knife-teaching.trycloudflare.com/";
+const API_BASE_URL = "https://elect-tube-legislative-casting.trycloudflare.com/";
 
 export async function apiFetch<T>(
   endpoint: string,
